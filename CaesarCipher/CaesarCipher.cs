b40b1f1274b780881e856a117a4e64883864e7fc
Prateek Sahay <prateeksahay96@gmail.com>
2018-09-16 23:36:09 +0530

    Using String Builder

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -1,13 +1,41 @@
 ﻿using System;
 using System.Linq;
+using System.Text;
 
 namespace CaesarCipher
 {
     public static class RotationalCipher
     {
+        
         public static string Rotate(string text, int shiftKey)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            // ASCII code for a -> 97 z -> 122 && A -> 65 Z -> 90
+            int upperAlphabet = 65;
+            int lowerAlphabet = 97;
+            
+            StringBuilder cipher = new StringBuilder();
+            int charInAscii = 0;
+            foreach(char c in text){
+                charInAscii = c;
+                if(charInAscii >= 'A' && charInAscii <= 'Z'){
+                    charInAscii = CaesarCipherHelper(charInAscii, upperAlphabet, shiftKey);
+                }
+                else if(charInAscii >= 'a' && charInAscii <= 'z'){
+                    charInAscii = CaesarCipherHelper(charInAscii, lowerAlphabet, shiftKey);
+                }
+                
+                char cipherChar = (char)charInAscii;
+                
+                cipher.Append(cipherChar);
+            }
+            return cipher.ToString();
+        }
+      
+        private static int CaesarCipherHelper(int charInAscii, int alphabetStart, int shiftKey){
+            charInAscii = charInAscii - alphabetStart + shiftKey;
+            charInAscii %= 26;
+            charInAscii += alphabetStart;
+            return charInAscii;
         }
     }
 }
